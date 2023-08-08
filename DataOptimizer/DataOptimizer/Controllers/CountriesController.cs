d2de61e80c40bbe0458c43b68440bd309cc2a603
Denys Yatsuliak <denys.yatsuliak@bankjoy.com>
2023-08-08 12:59:14 +0300

    UC#1 Create endpoint

---
+++ DataOptimizer/DataOptimizer/Controllers/CountriesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DataOptimizer.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class CountriesController : ControllerBase
+{
+    private readonly HttpClient _httpClient;
+
+    public CountriesController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClient = httpClientFactory.CreateClient();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetCountries(string? p1, int? p2, string? p3)
+    {
+        var result = await _httpClient.GetAsync("https://restcountries.com/v3.1/all");
+
+        return Ok(result.Content.ReadFromJsonAsync<object>());
+    }
+}
